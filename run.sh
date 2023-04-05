d7661504c8d10c75b2b10ecef948e9f488343902
Alex Rublev <aleks.rublev@gmail.com>
2023-04-05 13:18:17 +0600

    init

--- run.sh
+++ run.sh
@@ -6,13 +6,14 @@ vert_path=$PWD/$venv_name
 if [ ! -d "$vert_path" ]; then
     echo "Virtual environment will be created in this path: ${vert_path}"
     sleep 1
-    sudo apt-get update -y
-    sudo apt-get install -y python3-venv
+    #sudo apt-get update -y
+    #sudo apt-get install -y python3-venv
     python3 -m venv $vert_path
 fi
 
 source "${vert_path}/bin/activate"
+python --version
 pip install -r requirements.txt
-python run.py
+python run.py "$@"
 
 
